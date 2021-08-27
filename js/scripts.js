1cd9e72f52b011573c3e97a6254b36726f552f4b
Paige Tiedeman <paigetiedeman@gmail.com>
2021-08-27 11:03:51 -0700

    basic HTML format

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,11 @@ function Pizza(topping, size, crust) {
 Pizza.prototype.calculateCost = function(pizzaCost) {
   let plainPizza = "$15";
   let price;
-  if (this.topping === "cheese" || this.topping === "pepperoni" || this.topping === "mushrooms") {
+  if (this.topping === "1" || this.topping === "2" || this.topping === "3" || this.topping === "4") {
     return price = "$20";
   } else if (this.size >= 15) {
     return price = "$25"
-  } else if (this.crust === "gluten-free") {
+  } else if (this.crust === "6" || this.crust === "7") {
     return price = "$30"
   } else {
     return plainPizza;
