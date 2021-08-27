634c56f065d6480c8ec338f9c6d90b4041b96cbe
Paige Tiedeman <paigetiedeman@gmail.com>
2021-08-27 12:37:07 -0700

    WIP: want to add additional costs for  multiple toppings selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Allow the user to choose toppings and size for the pizza they'd like to order.
 
-// Business Logic 
+//Business Logic 
 function Pizza() {
   this.pizzas = {};
 }
@@ -12,30 +12,24 @@ function Pizza(topping, size, crust) {
 }
 
 Pizza.prototype.calculateCost = function (pizzaCost) {
-  let plainPizza = "$15";
-  let price;
-  if (
-    this.topping === "1" ||
-    this.topping === "2" ||
-    this.topping === "3" ||
-    this.topping === "4"
-  ) {
-    return (price = "$20");
+  let plainPizza = 15;
+  // let price = 15;
+  if (this.topping === "1" || this.topping === "2" || this.topping === "3" || this.topping === "4") {
+    return plainPizza = plainPizza += 2;
   } else if (this.size >= 15) {
-    return (price = "$25");
+    return plainPizza = plainPizza += 5;
   } else if (this.crust === "CS" || this.crust === "GF") {
-    return (price = "$30");
+    return plainPizza = plainPizza += 6;
   } else {
     return plainPizza;
   }
 };
 
-//let pizza = new Pizza("cheese", 20);
 // UI Logic
 let pizza = new Pizza();
 
 function displayPrice(pizzaCost) {
-  $(".price").html(pizzaCost.calculateCost());
+  $(".price").html("Your Pizza is $" + pizzaCost.calculateCost());
 }
 
 $(document).ready(function () {
@@ -47,7 +41,7 @@ $(document).ready(function () {
     $("#toppings option:selected").val("");
     $("input:radio[name=crustSize]:checked").val("");
     $("#crust option:selected").val("");
-    var pizzaCost = new Pizza(
+    let pizzaCost = new Pizza(
       selectedToppings,
       selectedSize,
       selectedCrust
